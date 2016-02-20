7bc15c20e905d2f21360163ff364be4e07850d19
Marco Ramirez <marco.ramirez@rzerocorp.com>
2016-02-20 13:11:12 -0600

    JWT Started and almost finished

--- api/common/config/params.php
+++ api/common/config/params.php
@@ -3,4 +3,7 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
+
+    'SERVER_NAME' => 'loschingones',
+    'JWT_ALGORITHM' => 'HS512',
 ];
