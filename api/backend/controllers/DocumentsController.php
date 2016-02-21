9d93b722426f236bc706fa8da49e04955eb73632
Marco Ramirez <marco.ramirez@rzerocorp.com>
2016-02-20 18:52:51 -0600

    Started Documents structure. It will be handled with cURL for CouchDB (stinky shit -.-)

---
+++ api/backend/controllers/DocumentsController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: RZEROSTERN
+ * Date: 20/02/16
+ * Time: 17:55
+ */
+
+namespace backend\controllers;
+
+
+
+class DocumentsController extends Controller {
+
+} 
